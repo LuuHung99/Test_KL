253dcb48ee1715768762e35972883eaefe0a7437
luuvanhung <lvhdev99@gmail.com>
2021-05-27 09:49:28 +0700

    update code

--- src/pages/dashbroad/products/sidebar.js
+++ src/pages/dashbroad/products/sidebar.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
 import "./css/product.css";
 import { Layout, Menu, Input } from "antd";
-import { MenuOutlined, CloseOutlined,SearchOutlined } from "@ant-design/icons";
-import {  Link, useRouteMatch } from "react-router-dom";
+import { MenuOutlined, CloseOutlined, SearchOutlined } from "@ant-design/icons";
+import { Link, useRouteMatch } from "react-router-dom";
+import Tools from "../../../Tools/Tools";
 
 import { dataFake } from "../../../services/tree";
 
@@ -13,38 +14,59 @@ const { SubMenu } = Menu;
 function Products(props) {
   let match = useRouteMatch();
   const [searchSidebar, setSearchSidebar] = useState("");
-  const menus = dataFake;
+  const [menus, setMenus] = useState(dataFake);
+  console.log(menus);
+
+  // useEffect(() => {
+  //   let results = [];
+
+  //   for (let i = 0; i < menus.children.length; i++) {
+  //     const oneChild = menus.filter((item) =>
+  //       item.children[i].title.toLowerCase().includes(searchSidebar)
+  //     );
+  //     results.push(oneChild);
+  //   }
+  // }, [searchSidebar]);
 
   //Side bar
   function renderProductList() {
     return (
       Array.isArray(menus) &&
-      menus.map((text) => {
-        if (text.subs.length > 0)
+      menus
+        .filter((val) => {
+          if (searchSidebar === "") {
+            return val;
+          } else if (
+            val.title.toLowerCase().includes(searchSidebar.toLowerCase())
+          ) {
+            return val;
+          }
+        })
+        .map((text) => {
+          if (text.subs.length > 0)
+            return (
+              <SubMenu key={text.id} title={text.title}>
+                {Array.isArray(text.subs) &&
+                  text.subs.map((item) => {
+                    return (
+                      <Menu.Item key={item.id} path={item.path}>
+                        <Link to={`${match.path}${item.path}`}>
+                          {item.title}
+                        </Link>
+                      </Menu.Item>
+                    );
+                  })}
+              </SubMenu>
+            );
           return (
-            <SubMenu key={text.id} title={text.title}>
-              {Array.isArray(text.subs) &&
-                text.subs.map((item) => {
-                  return (
-                    <Menu.Item
-                      key={item.id}
-                      path={item.path}
-                    >
-                      <Link to={`${match.path}${item.path}`}>{item.title}</Link>
-                    </Menu.Item>
-                  );
-                })}
-            </SubMenu>
+            <Menu.Item key={text.id} path={text.path}>
+              <Link to={`${match.path}${text.path}`}>{text.title}</Link>
+            </Menu.Item>
           );
-        return (
-          <Menu.Item key={text.id} path={text.path}>
-            <Link to={`${match.path}${text.path}`}>{text.title}</Link>
-          </Menu.Item>
-        );
-      })
+        })
     );
   }
- 
+
   return (
     <>
       {/* Sidebar */}
@@ -59,24 +81,31 @@ function Products(props) {
         <Sider className="sidebar_container">
           <div className="logo">
             <img src="images/male-farmer.svg" className="logo__img" alt="" />
-            <p>Hi Hung</p>
+            <p style={{ marginTop: 10, fontSize: 18 }}>Hi Hung</p>
           </div>
-          <form type="submit">
-            <Input
-              placeholder="Tìm kiếm"
-              prefix={<SearchOutlined />}
-              style={{
-                marginLeft: "5px",
-                marginRight: "5px",
-                marginBottom: "10px",
-                width: "95%",
-              }}
-              val={searchSidebar}
-              // onPressEnter={(value) => onchangeSearchSidebar(value)}
-              onChange={(e) => setSearchSidebar(e.target.value)}
-            />
-          </form>
-          <Menu mode="inline">{renderProductList()}</Menu>
+
+          <Input
+            type="text"
+            placeholder="Tìm kiếm"
+            prefix={<SearchOutlined style={{ fontSize: "20px" }} />}
+            style={{
+              padding: "10px",
+              width: "100%",
+              border: "none",
+              background: "#f9f9f9d9",
+              borderRadius: "999px",
+              color: "#000",
+              height: "auto",
+              marginTop: "70px",
+            }}
+            value={searchSidebar}
+            // onPressEnter={onPressEnter}
+            onChange={(e) => setSearchSidebar(e.target.value)}
+          />
+
+          <Menu mode="inline" style={{ marginTop: "10px" }}>
+            {renderProductList()}
+          </Menu>
         </Sider>
       </Layout>
       {props.children}
